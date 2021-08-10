eebc97110de6a4e5a1991cc9b336747ebff17fb0
Shipin <zhao.shipin@datablau.com.cn>
2021-08-10 11:44:21 +0800

    快速排序

--- src/base/algorithm/sort/QuickSort.java
+++ src/base/algorithm/sort/QuickSort.java
@@ -1,8 +1,38 @@
 package base.algorithm.sort;
 
-public class QuickSort implements Sort{
+public class QuickSort implements Sort {
     @Override
     public int[] sort(int[] arr) {
-        return null;
+        if (arr == null || arr.length == 0) {
+            return arr;
+        }
+        sort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    public void sort(int[] arr, int start, int end) {
+        if (end > arr.length || start >= end)
+            return;
+        int t = arr[start];
+        int i = start, j = end;
+        while (i < j) {
+//            print("", start, end, i, j);
+            while (i < j && arr[j] >= t) {
+                j--;
+            }
+            arr[i] = arr[j];
+            while (i < j && arr[i] < t) {
+                i++;
+            }
+            arr[j] = arr[i];
+        }
+        arr[i] = t;
+        sort(arr, 0, i - 1);
+        sort(arr, i + 1, end);
+
+    }
+
+    void print(String prefix, int start, int end, int i, int j) {
+        System.out.printf("%s: start= %d, end=%d, i=%d,j=%d\n", prefix, start, end, i, j);
     }
 }
--- src/base/algorithm/sort/SortTest.java
+++ src/base/algorithm/sort/SortTest.java
@@ -1,20 +1,52 @@
 package base.algorithm.sort;
 
+import com.sun.org.apache.regexp.internal.RE;
+
 import java.util.Arrays;
+import java.util.Random;
 
 public class SortTest {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 6, 2, 9, 8, 10, 32, 53, 22};
-        bubbling(copyOf(arr));
+        int[][] arrs = {
+                {7, 56, 80, 56, 80, 25, 80, 46, 87, 48, 83}
+        };
+        arrs = randSource();
+        Sort sort = new QuickSort();
+
+        for (int i = 0; i < arrs.length; i++) {
+            int[] arr = arrs[i];
+            int[] source = copyOf(arr);
+            int[] target = copyOf(arr);
+            Arrays.sort(target);
+
+            System.out.println("sort : " + Arrays.toString(source));
+            System.out.println("target : " + Arrays.toString(target));
+            int[] result = sort.sort(source);
+            System.out.println("result : " + Arrays.toString(result));
+            System.out.println();
+            if (!Arrays.equals(target, result)) {
+                System.out.println("error");
+                return;
+            }
+        }
+        System.out.println("测试通过");
     }
-    public static int [] copyOf(int [] arr){
+
+    public static int[] copyOf(int[] arr) {
         return Arrays.copyOf(arr, arr.length);
     }
 
-    public static void bubbling(int [] arr){
-        BubblingSort bubblingSort = new BubblingSort();
-        arr = bubblingSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+    public static int[][] randSource() {
+        Random random = new Random();
+        int n = 1000, max = 100, maxLength = 30;
+        int arrs[][] = new int[n][];
+        for (int i = 0; i < n; i++) {
+            int len = random.nextInt(maxLength);
+            arrs[i] = new int[len];
+            for (int j = 0; j < len; j++) {
+                arrs[i][j] = random.nextInt(max);
+            }
+        }
+        return arrs;
     }
-
 }
